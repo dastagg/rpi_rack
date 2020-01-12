940d91ef1ec19af8f56823d586ba6143d892c34d
dastagg(opus3) <dastagg@stagglinux.com>
2020-01-12 17:19:22 -0500

    Completed source_control role in Test

---
+++ test_platform/control/roles/source_control/files/fossil_files/mkrepo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then 
+  echo usage: $0 repository
+  exit
+fi
+REPO=$1
+
+fossil init $REPO.fsl
+fossil config import ticket.config -R $REPO.fsl
+
+chown www-data:www-data $REPO.fsl
+
+
